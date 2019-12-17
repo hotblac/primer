46c6d679a620a3d179807fdc542b65c085f5fcf7
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2019-12-17 15:26:14 +0000

    Use functional model to process stream. Revert to Spring Cloud Hoxton

--- display/src/main/java/org/dontpanic/primer/display/NumberStreamListener.java
+++ display/src/main/java/org/dontpanic/primer/display/NumberStreamListener.java
@@ -3,19 +3,20 @@ package org.dontpanic.primer.display;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Flux;
+
+import java.util.function.Consumer;
 
 @EnableBinding(Sink.class)
 public class NumberStreamListener {
 
     private Logger logger = LoggerFactory.getLogger(NumberStreamListener.class);
 
-    @StreamListener
-    public void accept(@Input(Sink.INPUT) Flux<Integer> numbers) {
-        numbers
+    @Bean
+    public Consumer<Flux<Integer>> accept() {
+        return flux -> flux
                 .map(n -> new PrimeCandidate(n, 0))
                 .subscribe(pc -> logger.info(pc.toString()));
     }

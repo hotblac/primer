ba7b0a3295b71a89d856b599c1ba61e265c8f424
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2019-12-19 14:19:34 +0000

    Prime checking logic

--- prime-checker/src/main/java/org/dontpanic/primer/primechecker/PrimeCheckerProcessor.java
+++ prime-checker/src/main/java/org/dontpanic/primer/primechecker/PrimeCheckerProcessor.java
@@ -1,5 +1,6 @@
 package org.dontpanic.primer.primechecker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.context.annotation.Bean;
@@ -9,9 +10,11 @@ import java.util.function.Function;
 @EnableBinding(Processor.class)
 public class PrimeCheckerProcessor {
 
+    @Autowired private PrimeChecker checker;
+
     @Bean
     public Function<Integer, PrimeCandidate> checkPrime() {
-        return i -> new PrimeCandidate(i, false);
+        return i -> new PrimeCandidate(i, checker.isPrime(i));
     }
 
 }
--- prime-checker/src/test/java/org/dontpanic/primer/primechecker/PrimeCheckerProcessorTest.java
+++ prime-checker/src/test/java/org/dontpanic/primer/primechecker/PrimeCheckerProcessorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.cloud.stream.test.binder.MessageCollector;
 import org.springframework.messaging.Message;
@@ -13,6 +14,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -20,11 +22,13 @@ class PrimeCheckerProcessorTest {
 
     @Autowired private Processor processor;
     @Autowired private MessageCollector messageCollector;
+    @MockBean private PrimeChecker checker;
 
     @Test
     void integerIn_primeCandidateOut() throws Exception {
+        when(checker.isPrime(42)).thenReturn(true);
         processor.input().send(MessageBuilder.withPayload("42").build());
         Message output = messageCollector.forChannel(processor.output()).poll(1, TimeUnit.SECONDS);
-        assertEquals("{\"number\":42,\"prime\":false}", output.getPayload());
+        assertEquals("{\"number\":42,\"prime\":true}", output.getPayload());
     }
 }

6a6816f0497f417b1d4929051992220e623870f0
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2019-12-19 12:59:42 +0000

    Generate prime-checker project from start.spring.io

---
+++ prime-checker/src/test/java/org/dontpanic/primer/primechecker/PrimeCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dontpanic.primer.primechecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

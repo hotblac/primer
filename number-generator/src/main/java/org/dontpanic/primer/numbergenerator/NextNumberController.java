665a0d7f4149f744b3cf75d4c1142a60ab6a159f
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2019-12-31 10:49:42 +0000

    Generate numbers on a schedule

--- number-generator/src/main/java/org/dontpanic/primer/numbergenerator/NextNumberController.java
+++ number-generator/src/main/java/org/dontpanic/primer/numbergenerator/NextNumberController.java
@@ -4,19 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class NextNumberController {
 
     @Autowired private NumberGenerator generator;
-    @Autowired private NumberSender sender;
 
-    @GetMapping("/next")
+    @GetMapping("/start")
     @ResponseStatus(value = HttpStatus.OK)
-    public void sendNextNumber() {
-        int next = generator.next();
-        sender.sendNumber(next);
+    public void start(@RequestParam(defaultValue = "0") int startAt) {
+        generator.start(startAt);
+    }
+
+    @GetMapping("/stop")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void stop() {
+        generator.stop();
     }
 
 

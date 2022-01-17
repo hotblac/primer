3ca991290d82bb83a0be806ecabbbce6d691bd4c
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2022-01-17 16:18:55 +0000

    Log received events at debug level

--- display/src/main/java/org/dontpanic/primer/display/FluxBuilder.java
+++ display/src/main/java/org/dontpanic/primer/display/FluxBuilder.java
@@ -1,5 +1,6 @@
 package org.dontpanic.primer.display;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
@@ -11,12 +12,14 @@ import java.util.Objects;
  * @param <T> type of item in the Flux
  */
 @Component
+@Slf4j
 public class FluxBuilder<T> {
 
     private FluxSink<T> fluxSink = null;
-    private Flux<T> flux = Flux.create(fluxSink -> this.fluxSink = fluxSink);
+    private final Flux<T> flux = Flux.create(fluxSink -> this.fluxSink = fluxSink);
 
     public void add(T item) {
+        log.debug("Received item for display: {}", item);
         if (!Objects.isNull(fluxSink)) {
             fluxSink.next(item);
         }
